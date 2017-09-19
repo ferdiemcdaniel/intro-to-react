58d481347935f0544bdf87cbc4a7e658ad2e9eb7
Ferdinand McDaniel <imcdanielamano@gmail.com>
2017-09-19 11:10:56 +0800

    updatedd

--- js/App.jsx
+++ js/App.jsx
@@ -1,5 +1,3 @@
-Ignored unknown option: --config-precedence
-
 // @flow
 
 import React from 'react';
@@ -11,40 +9,46 @@ import store from './store';
 import preload from '../data.json';
 
 const App = () => (
-  <Provider store={store}>
-    <div className="app">
-      <Route
-        exact
-        path="/"
-        component={props => (
-          <AsyncRoute props={props} loadingPromise={import('./Landing')} />
-        )}
-      />
-      <Route
-        path="/search"
-        component={props => (
-          <AsyncRoute
-            loadingPromise={import('./Search')}
-            props={Object.assign({shows: preload.shows}, props)}
-          />
-        )}
-      />
-      <Route
-        path="/details/:id"
-        component={(props: {match: Match}) => {
-          const selectedShow = preload.shows.find(
-            (show: Show) => props.match.params.id === show.imdbID,
-          );
-          return (
-            <AsyncRoute
-              loadingPromise={import('./Details')}
-              props={Object.assign({show: selectedShow, match: {}}, props)}
+    <Provider store={store}>
+        <div className="app">
+            <Route
+                exact
+                path="/"
+                component={props => (
+                    <AsyncRoute
+                        props={props}
+                        loadingPromise={import('./Landing')}
+                    />
+                )}
             />
-          );
-        }}
-      />
-    </div>
-  </Provider>
+            <Route
+                path="/search"
+                component={props => (
+                    <AsyncRoute
+                        loadingPromise={import('./Search')}
+                        props={Object.assign({shows: preload.shows}, props)}
+                    />
+                )}
+            />
+            <Route
+                path="/details/:id"
+                component={(props: {match: Match}) => {
+                    const selectedShow = preload.shows.find(
+                        (show: Show) => props.match.params.id === show.imdbID,
+                    );
+                    return (
+                        <AsyncRoute
+                            loadingPromise={import('./Details')}
+                            props={Object.assign(
+                                {show: selectedShow, match: {}},
+                                props,
+                            )}
+                        />
+                    );
+                }}
+            />
+        </div>
+    </Provider>
 );
 
 export default App;
