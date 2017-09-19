3e6f2f6b37ab10f7f15354a809fb88711404c693
Ferdinand McDaniel <imcdanielamano@gmail.com>
2017-09-19 10:16:42 +0800

    fixed error

--- js/AsyncRoute.jsx
+++ js/AsyncRoute.jsx
@@ -1,3 +1,5 @@
+Ignored unknown option: --config-precedence
+
 // @flow
 
 import React from 'react';
@@ -5,18 +7,18 @@ import Spinner from './Spinner';
 
 class AsyncRoute extends React.Component {
   state = {
-    loaded: false
+    loaded: false,
   };
   componentDidMount() {
     this.props.loadingPromise.then(module => {
       this.component = module.default;
-      this.setState({ loaded: true });
+      this.setState({loaded: true});
     });
   }
   component = null;
   props: {
     props: mixed,
-    loadingPromise: Promise<{ default: Class<React.Component<*, *, *>> }>
+    loadingPromise: Promise<{default: Class<React.Component<*, *, *>>}>,
   };
   render() {
     if (this.state.loaded) {
